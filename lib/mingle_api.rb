3970ea6a53f24095c8c2a398fbc6d92fbd23d526
Frank Trindade and Jon Spalding <frank.trindade@gmail.com>
2010-01-19 23:08:34 +0000

    mingle api returning current sprint cards

--- lib/mingle_api.rb
+++ lib/mingle_api.rb
@@ -8,9 +8,13 @@ class MingleApi
   def self.current_sprint(project_name)
     sprint_card = get("http://#{SERVER}/projects/#{project_name}/cards.xml?filters[]=[Type][is][sprint]&filters[]=[Sprint+End+Date][is+greater+than][(today)]&filters[]=[Sprint+Start+Date][is+less+than][(today)]")
     input = Nokogiri::XML(sprint_card)
-    sprint_name = input.root.xpath("/cards/card/name/text()").to_s
+    sprint_name = input.root.xpath("/cards/card/number/text()").to_s
     
     sprint_name.empty? ? nil : sprint_name
+  end
+  
+  def self.sprint_cards(project_name, sprint_name)
+    get("http://#{SERVER}/projects/#{project_name}/cards.xml?filters[]=[Type][is][subtask]&filters[]=[Sprint][is][#{sprint_name}]")
   end
   
 end
--- spec/mingle_api_spec.rb
+++ spec/mingle_api_spec.rb
@@ -2,11 +2,15 @@ require File.dirname(__FILE__) + '/spec_helper'
 
 describe MingleApi do
   
+  before :all do
+    @project_name = "sporting_bet_macro"
+    @base_url = "http://#{MingleApi::SERVER}/projects/#{@project_name}/cards.xml"
+  end
+  
   describe "current sprint" do
     
-    before :all do
-      @project_name = "sporting_bet_macro"
-      @expected_url = "http://#{MingleApi::SERVER}/projects/#{@project_name}/cards.xml" +
+    before :all do 
+      @expected_url = "#{@base_url}" +
       "?filters[]=[Type][is][sprint]" +
       "&filters[]=[Sprint+End+Date][is+greater+than][(today)]" +
       "&filters[]=[Sprint+Start+Date][is+less+than][(today)]"
@@ -14,7 +18,7 @@ describe MingleApi do
     
     it "should retrieve current sprint number based on today's date" do
       FakeWeb.register_uri(:get, "#{@expected_url}", :body => card_sprint)  
-      MingleApi.current_sprint(@project_name).should == "Sprint 3"
+      MingleApi.current_sprint(@project_name).should == "112"
     end
     
     it "should return null if we dont have a current sprint" do
@@ -24,13 +28,45 @@ describe MingleApi do
     
   end
   
+  describe "current_sprint_cards" do
+    
+    it "should find the current sprint cards" do
+      sprint_name = "112"
+      @expected_url = "#{@base_url}" +
+      "?filters[]=[Type][is][subtask]" + 
+      "&filters[]=[Sprint][is][#{sprint_name}]"
+      
+      FakeWeb.register_uri(:get, "#{@expected_url}", :body => sprint_set)  
+      MingleApi.sprint_cards(@project_name, sprint_name).should == sprint_set
+    end
+    
+  end
+  
+  def sprint_set
+    <<-XML
+    <cards type="array">
+      <card>
+        <card_type_name>SubTask</card_type_name>
+        <name>Blah</name>
+        <id>112</id>
+      </card>
+      <card>
+        <card_type_name>SubTask</card_type_name>
+        <name>Boo</name>
+        <id>114</id>
+      </card>
+    </cards>
+    XML
+  end
+  
   def card_sprint
     <<-XML
     <cards type="array">
       <card>
         <card_type_name>Sprint</card_type_name>
         <name>Sprint 3</name>
-      <card>
+        <number>112</number>
+      </card>
     </cards>
     XML
   end
